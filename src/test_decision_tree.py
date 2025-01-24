5a51f6484a5cfeb19bb0053764854275bb6479e3
Yan Lu <yan.lu@cnu.edu>
2025-01-24 23:26:27 +0000

    Upload New File

---
+++ src/test_decision_tree.py
@@ -0,0 +1,5 @@
+# Hardcode a small dataset, manually classify it by hand using a decision tree
+# Upload a photo of your manual classifications
+# Check that your code is correct.
+#    Be sure to check Entropy is correctly calculated, and _find_feature_which_best_splits is correct, and that
+#    the classifications are what you manually calculated
